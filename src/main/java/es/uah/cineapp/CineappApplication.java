9d1b52a9c928dd1f68fac58cc5c25c702a6c6c86
AlejandroGrueso <alejandro.grueso@edu.uah.es>
2025-12-31 18:50:24 +0100

    commit inicial

---
+++ src/main/java/es/uah/cineapp/CineappApplication.java
@@ -0,0 +1,13 @@
+package es.uah.cineapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/uah/cineapp/CineappApplicationTests.java
@@ -0,0 +1,13 @@
+package es.uah.cineapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
